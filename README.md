6276276f3c6a2bff43bd6195672067846b7f7b57
Shahab-16 <mdshahabuddin0516@gmail.com>
2024-04-07 16:24:58 +0530

    completed

--- README.md
+++ README.md
@@ -3,12 +3,7 @@
 ### Tools which we are going to use in this project
 
 1 [Github Account]("https://github.com/")
+
 2 [Visual Studio Code]("https://code.visualstudio.com/download")
 
-Create a new environment
 
----
-
-conda create -p venv python==3.7 -y
-
----
---
+++ app.py
@@ -0,0 +1,35 @@
+import pickle
+from flask import Flask,request,app,jsonify,url_for,render_template
+import numpy as np
+import pandas as pd
+
+app=Flask(__name__)
+regmodel=pickle.load(open('regmodel.pkl','rb'))
+scaler=pickle.load(open('scaling.pkl','rb'))
+
+@app.route('/')
+
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api',methods=['POST'])
+
+def predict_api():
+    data=request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data= scaler.transform(np.array(list(data.values())).reshape(1,-1))
+    output=regmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)
+    return render_template("home.html",prediction_text="The Predicted House Price is {}".format(output[0])) 
+
+if __name__=="__main__":
+    app.run(debug=True)
